b336523bff314dfc1a9f59db70b666d7108e24d0
ain <ain@dummy.dayo.com>
2015-10-09 22:59:45 +0900

    SpotInfを最新の型に合わせた

--- app/src/main/java/honda/onepoundsteakproject/SpotInf.java
+++ app/src/main/java/honda/onepoundsteakproject/SpotInf.java
@@ -1,16 +1,20 @@
 package honda.onepoundsteakproject;
-public class SpotInf {
-	public String name;
-	public double lat;
-    public double lng;
-    public double rate;
 
-	public  SpotInf(String name, double lat, double lng, double rate){
+
+public class SpotInf {
+    public int dataId;
+	public String name;
+	public float lat;
+    public float lng;
+    public float rate;
+    public float dist;
+
+	public  SpotInf(int dataId, String name, float lat, float lng, float rate, float dist){
+        this.dataId = dataId;
         this.name = name;
         this.lat = lat;
         this.lng = lng;
         this.rate = rate;
+        this.dist = dist;
     }
-	
-	
 }
