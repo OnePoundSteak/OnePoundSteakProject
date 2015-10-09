fe526079cd614d08c6d98947a7eaf995d0804af9
ain <ain@dummy.dayo.com>
2015-10-09 16:24:00 +0900

    ファイルを追加

---
+++ app/src/main/java/honda/onepoundsteakproject/AppController.java
@@ -0,0 +1,69 @@
+/**
+* Created by ain on 15/10/08.
+*/
+
+package com.example.ain.httptest;
+
+import android.text.TextUtils;
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+
+    public <T> void addToRequestQueue(JsonObjectRequest req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+
+    public <T> void addToRequestQueue(JsonArrayRequest req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
