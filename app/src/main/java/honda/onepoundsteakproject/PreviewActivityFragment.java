90cf01930dff87a80771316a4f4a760acdf446e1
ain <ain@dummy.dayo.com>
2015-10-12 02:04:10 +0900

    画像をロードできるようにした

--- app/src/main/java/honda/onepoundsteakproject/PreviewActivityFragment.java
+++ app/src/main/java/honda/onepoundsteakproject/PreviewActivityFragment.java
@@ -35,13 +35,12 @@ import java.util.ArrayList;
  * A placeholder fragment containing a simple view.
  */
 public class PreviewActivityFragment extends Fragment {
-
     private static final String TAG = PreviewActivityFragment.class.getSimpleName();
+    private String API_KEY = "AIzaSyBV6AkKjK5ZUYbU-ntP5-qKhSJMVuSJufY";
     private ArrayList<SpotInf> mSpotList;
     private TextView mSpotNameTextView;
     private TextView mSpotTimeTextView;
     private TextView mSpotFareTextView;
-
     private SpotInf mSpotInf;
     private String mSpotImageURL;
     private NetworkImageView mSpotImageView;
@@ -51,6 +50,7 @@ public class PreviewActivityFragment extends Fragment {
     private double mLon;
     private Boolean mSpotListLoaded;
     private Boolean mRouteListLoaded;
+    private Boolean mImageURLLoaded;
     private ProgressDialog mpDialog;
 
     public PreviewActivityFragment() {
@@ -67,7 +67,7 @@ public class PreviewActivityFragment extends Fragment {
         mSpotTimeTextView = null;
         mSpotFareTextView = null;
 
-        mSpotImageURL="";
+        mSpotImageURL = "";
         mMoney = getArguments().getInt("money");
         mTime = getArguments().getInt("time");
         mLon = getArguments().getDouble("lon");
@@ -78,6 +78,7 @@ public class PreviewActivityFragment extends Fragment {
 
         mSpotListLoaded = false;
         mRouteListLoaded = false;
+        mImageURLLoaded = false;
 
         spotListRequest(mLat, mLon, mTime);
     }
@@ -87,10 +88,10 @@ public class PreviewActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_preview, container, false);
 
-        mSpotNameTextView = (TextView)view.findViewById(R.id.spotNameText);
-        mSpotTimeTextView = (TextView)view.findViewById(R.id.spotTimeText);
-        mSpotFareTextView = (TextView)view.findViewById(R.id.spotFareText);
-        mSpotImageView = (NetworkImageView)view.findViewById(R.id.spotImageView);
+        mSpotNameTextView = (TextView) view.findViewById(R.id.spotNameText);
+        mSpotTimeTextView = (TextView) view.findViewById(R.id.spotTimeText);
+        mSpotFareTextView = (TextView) view.findViewById(R.id.spotFareText);
+        mSpotImageView = (NetworkImageView) view.findViewById(R.id.spotImageView);
 
         view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,11 +115,11 @@ public class PreviewActivityFragment extends Fragment {
             }
         });
 
-        if(mSpotInf != null){
+        if (mSpotInf != null) {
             mSpotNameTextView.setText(mSpotInf.name);
         }
 
-        if(!mSpotImageURL.equals("")){
+        if (!mSpotImageURL.equals("")) {
             AppController.getInstance().getRequestQueue();
             mSpotImageView.setImageUrl(mSpotImageURL, new ImageLoader(AppController.getInstance().getRequestQueue(), new BitmapCache()));
         }
@@ -126,77 +127,59 @@ public class PreviewActivityFragment extends Fragment {
         return view;
     }
 
-    private ArrayList<SpotInf> parseJSONtoSpotList(JSONObject jsondata){
-        ArrayList<SpotInf> ret = new ArrayList<SpotInf>();
 
-        try {
-            JSONArray spots = jsondata.getJSONArray("spots");
-            for(int i=0; i<spots.length(); i++) {
-                JSONObject spot = spots.getJSONObject(i);
-                ret.add(new SpotInf(
-                        Integer.parseInt(spot.getString("data_id")),
-                        spot.getString("name"),
-                        Float.parseFloat(spot.getString("lat")),
-                        Float.parseFloat(spot.getString("lon")),
-                        Float.parseFloat(spot.getString("rate")),
-                        Float.parseFloat(spot.getString("distance"))));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    private void spotListRequest(double lat, double lon, float time) {
+        String tag_json_obj = "json_obj_req";
+        String url = "https://gentle-basin-2840.herokuapp.com/place/" + lat + "/" + lon + "/" + time;
+        Log.d("Access URL:", url);
+        // ロード中表示
+        mpDialog.show();
 
-        Log.d("ListSize:", "" + ret.size());
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mSpotList = parseJSONtoSpotList(response);
+                        Log.d("spotList:", "loading comp!");
 
-        return ret;
-    }
+                        if (mSpotNameTextView != null && mSpotList.size() != 0) {
+                            // 本用はソートするべき
+                            mSpotInf = mSpotList.get(15);
+                        }
 
-    private ArrayList<RouteInf> parseJSONtoRouteList(JSONObject jsondata){
-        ArrayList<RouteInf> ret = new ArrayList();
-        // stateでOKかどうかも確認したほうがいいかも
-        try {
-            JSONArray routes = jsondata.getJSONArray("routes");
+                        // 経路情報の取得
+                        routeListRequest(mLat, mLon, mSpotInf.lat, mSpotInf.lon);
 
-            for(int i=0; i<routes.length(); i++) {
-                JSONObject route = routes.getJSONObject(i);
-                JSONArray legs = route.getJSONArray("legs");
-                JSONObject leg = legs.getJSONObject(0);
-                JSONArray steps = leg.getJSONArray("steps");
-                JSONObject step = steps.getJSONObject(0);
-                // --
-                JSONObject distance = step.getJSONObject("distance");
-                JSONObject duration = step.getJSONObject("duration");
-                int fare;
-                if(step.has("fare")){
-                    fare = step.getJSONObject("fare").getInt("value");
-                }else{
-                    fare = 0;
+                        mSpotListLoaded = true;
+                        if (mSpotListLoaded && mRouteListLoaded && mImageURLLoaded) {
+                            mpDialog.hide();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                mpDialog.hide(); // TODO ここもしっかり書き分けるべき
+
+                if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
                 }
-                JSONObject startLoc = step.getJSONObject("start_location");
-                JSONObject endLoc = step.getJSONObject("end_location");
-                // --
-                ret.add(new RouteInf(
-                        distance.getDouble("value")/1000, // mで返されているのでkmに換算
-                        duration.getInt("value")/60, // 秒で返されているので分に換算
-                        fare,
-                        startLoc.getDouble("lat"),
-                        startLoc.getDouble("lng"),
-                        endLoc.getDouble("lat"),
-                        endLoc.getDouble("lng")));
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        });
 
-        Log.d("ListSize:", "" + ret.size());
-
-        return ret;
+        // シングルトンクラスで実行
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
     }
 
-    private void directionRequest(double orgLat, double orgLon, double destLat, double destLon){
+    private void routeListRequest(double orgLat, double orgLon, double destLat, double destLon) {
         String tag_json_obj = "DirectionJson_obj_req";
-        String API_KEY = "AIzaSyBV6AkKjK5ZUYbU-ntP5-qKhSJMVuSJufY";
-        String url = "https://maps.googleapis.com/maps/api/directions/json?"+
-                "origin=" + orgLat +"," + orgLon +
+        String url = "https://maps.googleapis.com/maps/api/directions/json?" +
+                "origin=" + orgLat + "," + orgLon +
                 "&destination=" + destLat + "," + destLon +
                 "&key=" + API_KEY +
                 "&mode=transit&alternatives=true" +
@@ -214,7 +197,6 @@ public class PreviewActivityFragment extends Fragment {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("よんだ", "ぱーさー");
                         ArrayList<RouteInf> newRouteList = parseJSONtoRouteList(response);
                         Log.d("spotList:", "loading comp!");
                         Log.d(">>>>>>>", newRouteList.get(0).distance + ", "
@@ -229,15 +211,66 @@ public class PreviewActivityFragment extends Fragment {
 
                         // 画面に適用
                         mSpotNameTextView.setText(mSpotInf.name); // TODO 修正しなきゃ
-                        mSpotFareTextView.setText(newRouteList.get(0).fare+"円");
-                        mSpotTimeTextView.setText(newRouteList.get(0).duration+"分");
+                        mSpotFareTextView.setText(newRouteList.get(0).fare + "円");
+                        mSpotTimeTextView.setText(newRouteList.get(0).duration + "分");
 
+                        // TODO!!!!!!!!!!!
+                        imgURLRequest(mSpotInf.name);
+
+                        if (mSpotListLoaded && mRouteListLoaded && mImageURLLoaded) {
+                            mpDialog.hide();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                mpDialog.hide(); // TODO ここもしっかり書き分けるべき
+
+                if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                }
+            }
+        });
+
+        // シングルトンクラスで実行
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+    }
+
+    private void imgURLRequest(String keyword){
+        String tag_json_obj = "imgSearch_obj_req";
+        String url = "https://www.googleapis.com/customsearch/v1?"
+                + "key=" + API_KEY
+                + "&cx=" + "004164534463101160377:waf7n6e8twc"
+                + "&searchType=image"
+                + "&imgSize=large"
+                + "&imgType=photo"
+                + "&q=" + keyword;
+        Log.d("Access URL:", url);
+
+        // ロード中表示
+        mpDialog.show();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("よんだ", "いめーじぱーさー");
+                        String imgURL = parseJSONtoImageURL(response);
+                        Log.d("imgURL:", "loading comp!");
                         // 画像のロード
-                        mSpotImageURL = "http://www.jalan.net/jalan/img/9/spot/0109/KL/26106aa1020109571_1.jpg";
-                        AppController.getInstance().getRequestQueue();
-                        mSpotImageView.setImageUrl(mSpotImageURL, new ImageLoader(AppController.getInstance().getRequestQueue(), new BitmapCache()));
+                        if(!imgURL.equals("")) {
+                            AppController.getInstance().getRequestQueue();
+                            mSpotImageView.setImageUrl(imgURL, new ImageLoader(AppController.getInstance().getRequestQueue(), new BitmapCache()));
+                        }
+                        mImageURLLoaded = true;
 
-                        if(mSpotListLoaded && mRouteListLoaded) {
+                        if(mSpotListLoaded && mRouteListLoaded && mImageURLLoaded) {
                             mpDialog.hide();
                         }
                     }
@@ -261,54 +294,85 @@ public class PreviewActivityFragment extends Fragment {
         AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
     }
 
-    private void spotListRequest(double lat, double lon, float time) {
-        String tag_json_obj = "json_obj_req";
-        String url = "https://gentle-basin-2840.herokuapp.com/place/" + lat + "/" + lon + "/" + time;
-        Log.d("Access URL:", url);
-        // ロード中表示
-        mpDialog.show();
+    private ArrayList<SpotInf> parseJSONtoSpotList(JSONObject jsondata) {
+        ArrayList<SpotInf> ret = new ArrayList<SpotInf>();
 
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
-                url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        mSpotList = parseJSONtoSpotList(response);
-                        Log.d("spotList:", "loading comp!");
+        try {
+            JSONArray spots = jsondata.getJSONArray("spots");
+            for (int i = 0; i < spots.length(); i++) {
+                JSONObject spot = spots.getJSONObject(i);
+                ret.add(new SpotInf(
+                        Integer.parseInt(spot.getString("data_id")),
+                        spot.getString("name"),
+                        Float.parseFloat(spot.getString("lat")),
+                        Float.parseFloat(spot.getString("lon")),
+                        Float.parseFloat(spot.getString("rate")),
+                        Float.parseFloat(spot.getString("distance"))));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-                        if(mSpotNameTextView != null && mSpotList.size() != 0){
-                            // 本用はソートするべき
-                            mSpotInf = mSpotList.get(10);
-                        }
+        Log.d("ListSize:", "" + ret.size());
 
-                        // 経路情報の取得
-                        directionRequest(mLat, mLon, mSpotInf.lat, mSpotInf.lon);
-
-                        mSpotListLoaded = true;
-                        if(mSpotListLoaded && mRouteListLoaded) {
-                            mpDialog.hide();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        VolleyLog.d(TAG, "Error: " + error.getMessage());
-                        mpDialog.hide(); // TODO ここもしっかり書き分けるべき
-
-                        if( error instanceof NetworkError) {
-                        } else if( error instanceof ServerError) {
-                        } else if( error instanceof AuthFailureError) {
-                        } else if( error instanceof ParseError) {
-                        } else if( error instanceof NoConnectionError) {
-                        } else if( error instanceof TimeoutError) {
-                        }
-                    }
-        });
-
-        // シングルトンクラスで実行
-        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+        return ret;
     }
 
+    private ArrayList<RouteInf> parseJSONtoRouteList(JSONObject jsondata) {
+        ArrayList<RouteInf> ret = new ArrayList();
+        // stateでOKかどうかも確認したほうがいいかも
+        try {
+            JSONArray routes = jsondata.getJSONArray("routes");
 
+            for (int i = 0; i < routes.length(); i++) {
+                JSONObject route = routes.getJSONObject(i);
+                JSONArray legs = route.getJSONArray("legs");
+                JSONObject leg = legs.getJSONObject(0);
+                JSONArray steps = leg.getJSONArray("steps");
+                JSONObject step = steps.getJSONObject(0);
+                // --
+                JSONObject distance = step.getJSONObject("distance");
+                JSONObject duration = step.getJSONObject("duration");
+                int fare;
+                if (step.has("fare")) {
+                    fare = step.getJSONObject("fare").getInt("value");
+                } else {
+                    fare = 0;
+                }
+                JSONObject startLoc = step.getJSONObject("start_location");
+                JSONObject endLoc = step.getJSONObject("end_location");
+                // --
+                ret.add(new RouteInf(
+                        distance.getDouble("value") / 1000, // mで返されているのでkmに換算
+                        duration.getInt("value") / 60, // 秒で返されているので分に換算
+                        fare,
+                        startLoc.getDouble("lat"),
+                        startLoc.getDouble("lng"),
+                        endLoc.getDouble("lat"),
+                        endLoc.getDouble("lng")));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("ListSize:", "" + ret.size());
+
+        return ret;
+    }
+
+    private String parseJSONtoImageURL(JSONObject jsondata) {
+        String ret = ""; // TODO 検索できなかった場合を考えておくべき
+
+        try {
+            JSONObject item = jsondata.getJSONArray("items").getJSONObject(0);
+            ret = item.getString("link");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("imgURL:", ret);
+
+        return ret;
+    }
 
 }
